b88628c96c8d2fffb4c7677febf00e8802731312
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-11-22 17:50:11 -0300

    Enunciado do exercício de dicionário adicionado.

---
+++ codigos-fonte/10-Colecoes_parte_3-Dicionario/exercicios/exercicio_01.py
@@ -0,0 +1,4 @@
+'''
+1. Em programação, chamamos de sistema ***CRUD*** um sistema que faça 4 operações: ***C***_reate_, ***R***_ead_, ***U***_pdate_ e ***D***_elete_, que significam, respectivamente: cadastrar, pesquisar, atualizar e excluir. Crie um ***CRUD***, ou seja, um sistema que faça essas 4 operações em uma lista de dicionário.
+'''
+
