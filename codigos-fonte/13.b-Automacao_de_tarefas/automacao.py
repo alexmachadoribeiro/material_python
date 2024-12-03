5562e06ea2befb402084222ca0ef6b6df434ef96
alexmachadoribeiro <alex.ribeiro@df.docente.senai.br>
2024-12-03 14:38:19 -0300

    Atualizado: imagens em Automação de tarefas.

---
+++ codigos-fonte/13.b-Automacao_de_tarefas/automacao.py
@@ -0,0 +1,27 @@
+# importa bibliotecas
+import pyautogui
+import time
+
+# tempo que cada comando demora para executar
+pyautogui.PAUSE = 1
+
+# instruções
+pyautogui.press('win')
+pyautogui.write('vscode')
+pyautogui.press('enter')
+
+# espera 10 segundos para abrir o vscode e continuar com os comandos
+time.sleep(10)
+
+# continua as instruções
+pyautogui.hotkey('ctrl', 'shift', "'")
+pyautogui.write('git add .')
+pyautogui.press('enter')
+pyautogui.write('git commit -m "Repositório atualizado por automação."')
+
+# espera 5 segundos para dar tempo de fazer o commit
+time.sleep(5)
+
+# continua as instruções
+pyautogui.press('enter')
+pyautogui.write('git push')
